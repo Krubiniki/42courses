aa9c8e522f49e5e32bda794de833581c7466fba4
Krubinki <mkrubini@KrubiPC.localdomain>
2020-03-13 00:43:49 -0300

    split done! 13/03

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -13,28 +13,35 @@
 #include "libft.h"
 #include <stdlib.h>
 
-char	**cutstring(char **split, char *str, int count)
+char	**cutstring(char **split, char *str, char c, int cut)
 {
 	int index;
 	int cut2;
 	int start;
+	int count;
 
-	index = -1;	
+	index = 0;	
 	cut2 = 0;
-	while (str[count] != '\0')
+	start = 0;
+	count = 0;
+	while (str[count] != '\0' && cut2 < cut)
 	{
 		while (str[count] == c)
 			count++;
 		if (str[count] != c)
 			start = count;
-		while (str[count] != c)
+		while (str[count] != c && str[count] != '\0')
 			count++;
 		if (!(split[cut2] = malloc(sizeof(char) * ((count - start) + 1))))
 			return (NULL);
-		while (++index < (count - start))
+		while ((start + index) < count)
+		{
 			split[cut2][index] = str[index + start];
+			index++;
+		}
 		split[cut2][index] = '\0';
 		cut2++;
+		index = 0;
 	}
 	split[cut2] = NULL;
 	return (split);
@@ -52,17 +59,16 @@ char	**ft_split(char const *s, char c)
 	count = 0;
 	str = (char *)s;
 	cut = 0;
+	
 	while (str[count] != '\0')
 	{
-		while (str[count] == c && str[count] != '\0')
+		while (str[count] == c && str[count + 1] != '\0')
 			count++;
-		if ((str[count - 1] == c && str[count] != c) 
-						|| (count == 0 && str[count] != c) )
+		if ((count == 0 && str[count] != c) || (str[count - 1] == c && str[count] != c)) 
 			cut++;
 		count++;
 	}
 	if (!(split = malloc(sizeof(char *) * (cut + 1))))
-			return (NULL);
-	count = 0;
-	return (cutstring(split,str,count));
+		return (NULL);
+	return (cutstring(split, str, c, cut));
 }
